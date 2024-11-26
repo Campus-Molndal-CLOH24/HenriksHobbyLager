0a33fd9665bda6463c46ac237db5731ba0da8d13
Hussein Mohammadi <hussain.muhammadi.1234@gmail.com>
2024-11-26 12:08:50 +0100

    test

--- HenriksHobbyLager/Interfaces/IRepository.cs
+++ HenriksHobbyLager/Interfaces/IRepository.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace HenriksHobbyLager.Interfaces
 {
-    internal interface IRepository
+    public interface IRepository<T>
     {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        IEnumerable<T> Search(Func<T, bool> predicate);
     }
 }
