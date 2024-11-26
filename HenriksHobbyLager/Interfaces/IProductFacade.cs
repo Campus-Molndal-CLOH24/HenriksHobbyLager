ffaa0095cb4ec2fa6586672ad726461798883456
Yotaka k. <88yotkha@gafe.molndal.se>
2024-11-26 23:23:03 +0100

    fix: try to test if connecting work but it shows build solution failed

--- HenriksHobbyLager/Interfaces/IProductFacade.cs
+++ HenriksHobbyLager/Interfaces/IProductFacade.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using HenriksHobbyLager.Models;
 
 public interface IProductFacade
 {
--- HenriksHobbyLager/Program.cs
+++ HenriksHobbyLager/Program.cs
@@ -2,25 +2,32 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using Microsoft.Data.Sqlite;
+using HenriksHobbyLager.Database;
 
-namespace RefactoringExercise
+namespace HenriksHobbyLager
 {
-    class Program
+    public class Program
     {
+        static void Main(string[] args) 
+        {
+            using var connection = new SqliteConnection("Data Source=HobbyLager.db");
+            connection.Open();
+            Console.WriteLine("SQLite connection successful!");
+            /*
+            // Min fantastiska databas! Fungerar perfekt så länge datorn är igång
+            private static List<Product> _products = new List<Product>();
 
-        using var connection = new SqliteConnection("Data Source=HobbyLager.db");
-        connection.Open();
-        Console.WriteLine("SQLite connection successful!");
-        /*
-        // Min fantastiska databas! Fungerar perfekt så länge datorn är igång
-        private static List<Product> _products = new List<Product>();
-        
-        // Räknare för ID. Börjar på 1 för att 0 känns så negativt
-        private static int _nextId = 1;
+            // Räknare för ID. Börjar på 1 för att 0 känns så negativt
+            private static int _nextId = 1;
 
-        
-        */
-     }
+
+            */
+
+        }
+
+    }
         
 
         
