851bcfa479341b30aef6ac09b203a6562ed7fa8e
99amiqur <99amiqur@gafe.molndal.se>
2024-12-03 13:12:41 +0100

    Renkodat efter marcus lektion

---
+++ HenriksHobbyLager/Interfaces/IListRepository.cs
@@ -0,0 +1,15 @@
+﻿using HenriksHobbyLager.Models;
+
+namespace HenriksHobbyLager.Interfaces
+{
+    internal interface IListRepository
+    {
+        void AddProduct();
+        void DeleteProduct();
+        void DisplayProduct(Product product);
+        bool MakeLowerCaseCategory(Product p, string? searchTerm);
+        void SearchProducts();
+        void ShowAllProducts();
+        void UpdateProduct();
+    }
+}
